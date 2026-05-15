d022e8b4a2bdd04da15d5ee1f540f5f3acc0be0b
Ritesh Kumar <ritesh@emerald.pink>
2026-05-15 20:01:32 +0800

    [POC] Wire up multi-thread coordination via mvar

--- lib/client.ml
+++ lib/client.ml
@@ -1,7 +1,62 @@
 open Lwt
 open Lwt.Infix
 
-let echo_client host port =
+let read_stdin_loop queue =
+  let rec loop () =
+    let%lwt line = Lwt_io.read_line Lwt_io.stdin in
+    let%lwt () = Lwt_mvar.put queue line in
+    loop ()
+  in
+  loop ()
+
+let read_socket_loop input =
+  let rec loop () =
+    let%lwt line_opt = Lwt_io.read_line_opt input in
+    match line_opt with
+    | None ->
+        let%lwt () = Lwt_io.printl "[Server closed connection]" in
+        Lwt.fail End_of_file
+    | Some line ->
+        let%lwt () = Lwt_io.printlf "<server>: %s\n" line in
+        loop ()
+  in
+  loop ()
+
+let write_socket_loop output queue =
+  let rec loop () =
+    let%lwt line = Lwt_mvar.take queue in
+    let%lwt () = Lwt_io.write_line output line in
+    let%lwt () = Lwt_io.flush output in
+    loop ()
+  in
+  loop ()
+
+let handle_chat_session input output =
+  let queue = Lwt_mvar.create_empty () in
+  try%lwt
+    Lwt.join
+      [
+        read_stdin_loop queue;
+        read_socket_loop input;
+        write_socket_loop output queue;
+      ]
+  with
+  | End_of_file ->
+      let%lwt () = Lwt_io.printl "[Disconnected]" in
+      Lwt.return ()
+  | Unix.Unix_error (err, _, _) ->
+      let%lwt () =
+        Lwt_io.eprintf "Socket error: %s\n" (Unix.error_message err)
+      in
+      Lwt.fail (Failure (Unix.error_message err))
+  | e ->
+      let%lwt () =
+        Lwt_io.eprintf "Unexpected error: %s\n" (Printexc.to_string e)
+      in
+      Lwt.fail e
+
+let start_chat host port =
+  let%lwt () = Lwt_io.printlf "Connecting to %s:%d..." host port in
   let%lwt addr_info =
     Lwt_unix.getaddrinfo host (string_of_int port) [ Unix.(AI_FAMILY PF_INET) ]
   in
@@ -10,22 +65,26 @@ let echo_client host port =
     | [] -> Lwt.fail_with "failed to resolve host"
     | addr :: _ -> Lwt.return addr.Unix.ai_addr
   in
+
   let socket = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
   let%lwt () = Lwt_unix.connect socket addr in
+
   let input = Lwt_io.of_fd ~mode:Lwt_io.input socket in
   let output = Lwt_io.of_fd ~mode:Lwt_io.output socket in
-  let rec send_messages () =
-    let%lwt message = Lwt_io.read_line Lwt_io.stdin in
-    let%lwt () = Lwt_io.write_line output message in
-    let%lwt response = Lwt_io.read_line input in
-    let%lwt () = Lwt_io.printlf "Server replied: %s" response in
-    send_messages ()
+
+  let%lwt () = Lwt_io.printl "Connected successfully!" in
+
+  let handler_thunk = fun () -> handle_chat_session input output in
+  let cleaner_thunk =
+   fun () ->
+    let%lwt () = Lwt_io.close input in
+    let%lwt () = Lwt_io.close output in
+    Lwt_io.printl "Connection resources cleaned up"
   in
-  send_messages ()
+  Lwt.finalize handler_thunk cleaner_thunk
 
 let run ~host ~port ~timeout ~log_level =
-  let%lwt () = Lwt_io.printlf "Connecting to %s:%d\n" host port in
-  try%lwt echo_client host port
+  try%lwt start_chat host port
   with e ->
     let%lwt () = Lwt_io.eprintf "Client error: %s\n" (Printexc.to_string e) in
     Lwt.fail e
--- lib/server.ml
+++ lib/server.ml
@@ -13,26 +13,81 @@
 open Lwt
 open Lwt.Infix
 
-let handle_client (input, output) =
-  let rec echo_loop () =
+let read_stdin_loop queue =
+  let rec loop () =
+    let%lwt line = Lwt_io.read_line Lwt_io.stdin in
+    let%lwt () = Lwt_mvar.put queue line in
+    loop ()
+  in
+  loop ()
+
+let read_socket_loop input =
+  let rec loop () =
     let%lwt line_opt = Lwt_io.read_line_opt input in
     match line_opt with
-    | None -> Lwt_io.printl "Client disconnected"
+    | None ->
+        (* Socket closed *)
+        let%lwt () = Lwt_io.printl "[Connection closed by peer]" in
+        Lwt.fail End_of_file
     | Some line ->
-        let%lwt () = Lwt_io.printl ("Received: " ^ line) in
-        let%lwt () = Lwt_io.write_line output line in
-        echo_loop ()
+        let%lwt () = Lwt_io.printlf "<client>: %s\n" line in
+        loop ()
   in
-  echo_loop ()
+  loop ()
 
-let rec accept_connections server_socket =
-  let%lwt client_socket, _addr = Lwt_unix.accept server_socket in
+let write_socket_loop output queue =
+  let rec loop () =
+    let%lwt line = Lwt_mvar.take queue in
+    let%lwt () = Lwt_io.write_line output line in
+    let%lwt () = Lwt_io.flush output in
+    loop ()
+  in
+  loop ()
+
+let handle_client_conc input output =
+  let queue = Lwt_mvar.create_empty () in
+  try%lwt
+    Lwt.join
+      [
+        read_stdin_loop queue;
+        read_socket_loop input;
+        write_socket_loop output queue;
+      ]
+  with
+  | End_of_file ->
+      let%lwt () = Lwt_io.printl "[EOF: Connection terminated]" in
+      Lwt.return ()
+  | Unix.Unix_error (err, _, _) ->
+      let%lwt () =
+        Lwt_io.eprintf "Socket error: %s\n" (Unix.error_message err)
+      in
+      Lwt.fail (Failure (Unix.error_message err))
+  | e ->
+      let%lwt () =
+        Lwt_io.eprintf "Unexpected error: %s\n" (Printexc.to_string e)
+      in
+      Lwt.fail e
+
+let rec accept_loop server_socket =
+  let%lwt client_socket, _client_addr = Lwt_unix.accept server_socket in
+
   let input = Lwt_io.of_fd ~mode:Lwt_io.input client_socket in
   let output = Lwt_io.of_fd ~mode:Lwt_io.output client_socket in
-  Lwt.async (fun () -> handle_client (input, output));
-  accept_connections server_socket
+
+  let%lwt () = Lwt_io.printlf "Client connected\n" in
+  let handler_thunk = fun () -> handle_client_conc input output in
+  let cleaner_thunk =
+   fun () ->
+    let%lwt () = Lwt_io.close input in
+    let%lwt () = Lwt_io.close output in
+    Lwt_io.printl "Client cleaned up"
+  in
+  Lwt.finalize handler_thunk cleaner_thunk >>= fun () ->
+  Lwt_io.printl "Waiting for next client..." >>= fun () ->
+  accept_loop server_socket
 
 let start_server port bind =
+  let backlog_capacity = 1 in
   let inet_addr =
     try Unix.inet_addr_of_string bind
     with _ -> failwith (Printf.sprintf "Invalid bind address: %s" bind)
@@ -41,9 +96,9 @@ let start_server port bind =
   let server_socket = Lwt_unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
   Lwt_unix.setsockopt server_socket Unix.SO_REUSEADDR true;
   let%lwt () = Lwt_unix.bind server_socket sockaddr in
-  Lwt_unix.listen server_socket 10;
+  Lwt_unix.listen server_socket backlog_capacity;
   let%lwt () = Lwt_io.printlf "Server started on %s:%d\n" bind port in
-  accept_connections server_socket
+  accept_loop server_socket
 
 let run ~port ~bind ~timeout ~log_level =
   let%lwt () = Lwt_io.printlf "Starting server on %s:%d\n" bind port in
