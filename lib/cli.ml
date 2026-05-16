2dda567af65954e314dd017daf8872e2166a19eb
Ritesh Kumar <ritesh@emerald.pink>
2026-05-16 17:24:02 +0800

    Simplify cli, group server & client cmds together
    Just a cleanup before starting serious work. It's alright to keep them
    together, isn't too cluttered yet and they share common arguments. Can
    consider separating again if there's a need to add more terms...

--- lib/cli.ml
+++ lib/cli.ml
@@ -1,8 +1,83 @@
+(* TODO: add arg validation:
+   - e.g. port should be safe, use regex for hostname/ip addr (ref RFC) *)
+(* TODO: consider better error handling *)
+
 open Cmdliner
+
+let timeout_arg =
+  let default = 30 in
+  let doc =
+    "Connection timeout in seconds (default: " ^ Int.to_string default ^ ")"
+  in
+  Arg.(value & opt int 30 & info [ "timeout" ] ~docv:"SECONDS" ~doc)
+
+let log_level_arg =
+  let open Logs in
+  let levels =
+    [ Debug; Info; Warning; Error ]
+    |> List.map (fun level -> (Some level |> level_to_string, level))
+  in
+  let default_level = Info in
+  let levels_str = levels |> List.map fst |> String.concat ", " in
+  let default_str = Some default_level |> level_to_string in
+  let doc =
+    Printf.sprintf "Logging level: %s (default: %s)" levels_str default_str
+  in
+  Arg.(
+    value
+    & opt (enum levels) default_level
+    & info [ "log-level" ] ~docv:"LEVEL" ~doc)
+
+let port_server_arg =
+  let default = 4242 in
+  let doc =
+    "Server port to listen on (default " ^ Int.to_string default ^ ")"
+  in
+  Arg.(value & opt int default & info [ "port"; "p" ] ~docv:"PORT" ~doc)
+
+let bind_arg =
+  let default = "127.0.0.1" in
+  let doc = "Bind address (default: " ^ default ^ ")" in
+  Arg.(value & opt string default & info [ "bind" ] ~docv:"ADDR" ~doc)
+
+let port_client_arg =
+  let default = 4242 in
+  let doc =
+    "Server port to connect to (default: " ^ Int.to_string default ^ ")"
+  in
+  Arg.(value & opt int default & info [ "port"; "p" ] ~docv:"PORT" ~doc)
+
+let host_arg =
+  let default = "127.0.0.1" in
+  let doc = "Server hostname or IP (default: " ^ default ^ ")" in
+  Arg.(value & opt string default & info [ "host" ] ~docv:"HOST" ~doc)
+
+let run_server port bind timeout log_level =
+  try Lwt_main.run (Server.run ~port ~bind ~timeout ~log_level)
+  with e ->
+    Lwt_io.eprintf "Server error: %s\n%!" (Printexc.to_string e) |> Lwt_main.run;
+    exit 1
+
+let run_client host port timeout log_level =
+  try Lwt_main.run (Client.run ~host ~port ~timeout ~log_level)
+  with e ->
+    Lwt_io.eprintf "Client error: %s\n%!" (Printexc.to_string e) |> Lwt_main.run;
+    exit 1
 
 let cmd =
   Cmd.group
     (Cmd.info "chat" ~version:"0.1.0" ~doc:"Simple one-on-one chat application")
-    [ Cmd_server.cmd; Cmd_client.cmd ]
+    [
+      Cmd.make
+        (Cmd.info "server" ~doc:"Start a chat server")
+        Term.(
+          const run_server $ port_server_arg $ bind_arg $ timeout_arg
+          $ log_level_arg);
+      Cmd.make
+        (Cmd.info "client" ~doc:"Connect to a chat server")
+        Term.(
+          const run_client $ host_arg $ port_client_arg $ timeout_arg
+          $ log_level_arg);
+    ]
 
 let run () = exit (Cmd.eval cmd)
